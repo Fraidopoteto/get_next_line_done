db2f2cfbcd6658a79ef4dc3d03a269f16f9dba88
Fraidopoteto <140648485+Fraidopoteto@users.noreply.github.com>
2023-10-26 18:25:26 +0200

    Add files via upload

---
+++ get_next_line.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jschmunk <jschmunk@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/05 17:53:12 by jschmunk          #+#    #+#             */
+/*   Updated: 2023/10/26 17:32:03 by jschmunk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <string.h>
+# include <libc.h>
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *str);
+char	*ft_strchr(const char *str, int c);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_readbuff(int fd, char *save);
+char	*ft_getline(char *save);
+char	*ft_restofline(char *save);
+int		ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+int		ft_firstreadbuff(int fd, char *save);
+
+#endif
